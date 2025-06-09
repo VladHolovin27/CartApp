a66a79fc369f84178286f2b3af0b7eadf3ed1315
VladHolovin27 <vladislavg899@gmail.com>
2025-06-09 16:11:24 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import ProductsList from './components/ProductsList/ProductsList';
 import Cart from './components/Cart/Cart';
 
 
 function App() {
   return (
-      <Router>
+    <>
       <nav className='list-none flex gap-10 items-center justify-center'>
         <Link className='text-xl text-red-500 font-extrabold' to="/">Products</Link>
         <Link className='text-xl text-red-500 font-extrabold' to="/cart">Cart</Link>
@@ -16,8 +16,9 @@ function App() {
         <Route path="/" element={<ProductsList />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
-    </Router>
+    </>
   );
 }
+
 
 export default App
